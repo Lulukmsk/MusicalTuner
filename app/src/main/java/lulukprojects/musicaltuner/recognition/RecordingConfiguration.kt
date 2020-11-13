c977c73b7e013a7375eb2c4d3da917a4f8eecfd4
Luluk <lulukmsk@yandex.ru>
2020-11-13 14:22:42 +0300

    Added interpolation for Frequency recogniser. Added cut samples results to impove recognition speed. Refactor config for sampling information transer to the recorder and the recogniser.

--- app/src/main/java/lulukprojects/musicaltuner/recognition/RecordingConfiguration.kt
+++ app/src/main/java/lulukprojects/musicaltuner/recognition/RecordingConfiguration.kt
@@ -3,11 +3,12 @@ package lulukprojects.musicaltuner.recognition
 import android.media.AudioFormat
 import android.media.MediaRecorder
 
-data class RecrodingConfiguration(val samplingRate : Int) {
+data class RecrordingConfiguration(val samplingRate : Int) {
     val audioSource : Int = MediaRecorder.AudioSource.MIC
     val channelConfig: Int = AudioFormat.CHANNEL_IN_DEFAULT
     val audioFormat: Int = AudioFormat.ENCODING_PCM_16BIT
 
-    val samplingCutter: Double = samplingRate.toDouble() / 4
+    val samplingCutter: Double = samplingRate.toDouble() / 2
     val samplingMultiplier: Double = samplingRate.toDouble() * 16
+    val powerSpectrumNoise = 10000000
 }
